9a84bb0a2e85674f164bd47a9992669cc1fe372e
LachhabMeriem <121290183+LachhabMeriem@users.noreply.github.com>
2024-03-31 12:30:11 +0000

    add some modification

--- lib/drawingboxes.dart
+++ lib/drawingboxes.dart
@@ -6,47 +6,75 @@ class BndBox extends StatelessWidget {
   final int screenX;
   final int screenY;
   List<String>? _labels;
+  final image;
 
-  BndBox(this.results, this._labels, this.screenX, this.screenY);
+  BndBox(this.results, this._labels, this.screenX, this.screenY, this.image);
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> _renderBoxes() {
-      return results?.map((re) {
-        var left = re.left * (screenX / 640);
-        var top = re.top * (screenY / 640);
-        var right = re.right * (screenX / 640);
-        var bottom = re.bottom * (screenY / 640);
-        if (_labels?[re.maxClsIdx] != null && re.maxClsConfidence > 0.85) {
-          return Positioned(
-            left: left,
-            top: top,
-            child: Container(
-              padding: EdgeInsets.all(5.0),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Color.fromRGBO(37, 213, 253, 1.0),
-                  width: 3.0,
-                ),
-              ),
-              child: Text(
-                "${_labels?[re.maxClsIdx]} ${(re.maxClsConfidence * 100).toStringAsFixed(0)}%",
-                style: TextStyle(
-                  color: Color.fromRGBO(37, 213, 253, 1.0),
-                  fontSize: 14.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          );
-        } else {
-          return SizedBox.shrink();
-        }
-      }).toList() ?? [];
-    }
-
-    return Stack(
-      children: _renderBoxes(),
+    return CustomPaint(
+      painter: BoundingBoxPainter(results, screenX, screenY, _labels, image),
+      child: Container(
+        width: screenX.toDouble(),
+        height: screenY.toDouble(),
+        child: Image.memory(image),
+      ),
     );
+  }
+}
+
+class BoundingBoxPainter extends CustomPainter {
+  final List<BoundingBox>? results;
+  final int screenX;
+  final int screenY;
+  final List<String>? labels;
+  final image;
+
+  BoundingBoxPainter(this.results, this.screenX, this.screenY, this.labels, this.image);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    if (results != null) {
+      final double scaleX = size.width / screenX;
+      final double scaleY = size.height / screenY;
+
+      for (final box in results!) {
+        final left = box.left * scaleX;
+        final top = box.top * scaleY;
+        final right = box.right * scaleX;
+        final bottom = box.bottom * scaleY;
+        final width = right - left;
+        final height = bottom - top;
+
+        final Paint paint = Paint()
+          ..color = Colors.red
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 2.0;
+
+        canvas.drawRect(Rect.fromLTWH(left, top, width, height), paint);
+
+        final textStyle = TextStyle(
+          color: Colors.red,
+          fontSize: 14,
+          fontWeight: FontWeight.bold,
+        );
+        final textSpan = TextSpan(
+          text: labels?[box.maxClsIdx] ?? 'Unknown',
+          style: textStyle,
+        );
+        final textPainter = TextPainter(
+          text: textSpan,
+          textAlign: TextAlign.left,
+          textDirection: TextDirection.ltr,
+        );
+        textPainter.layout();
+        textPainter.paint(canvas, Offset(left, top - 20));
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
